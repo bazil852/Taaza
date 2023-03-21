4b2294e31b21cf4d678b175744548e32ad261607
bazil852 <60265910+bazil852@users.noreply.github.com>
2023-03-21 16:22:38 +0500

    Add files via upload

--- multiprocess.py
+++ multiprocess.py
@@ -1,5 +1,6 @@
 import csv
 import time
+#pyopenxl
 from collections import ChainMap
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,96 +13,102 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import multiprocessing as mp
 
-
-def scrape_product(item):
+def scrape_product(args):
+    item, account, login_inputs, login_button, cookie_button,Table_click, Table_input, Table_first_element = args
+    email, password=account
+    print(email,password)
     chrome_options = Options()
+    chrome_options.add_argument('--window-size=1280,720')
     # chrome_options.add_argument('--headless')
     driver = webdriver.Chrome('./chromedriver', options=chrome_options)
     wait = WebDriverWait(driver, 10)
     driver.get("https://www.tridge.com/login")
-    # time.sleep(2)
-    dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH,"//*[@class='sc-dMVFSy kkcmxE Polaris-TextField__Input']")))
+    dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH,"//*[@class='"+login_inputs+"']")))
     if (len(dummy) > 0):
-        dummy[0].send_keys("raoali1525@gmail.com")
-    else:
-        print("Couldn't find input")
+        dummy[0].send_keys(email)
+        print('passed')
 
-    # dummy=driver.find_element(By.XPATH,"//*[@class='sc-hAiVDd bbXFJJ Polaris-TextField__Input']")
-    dummy[1].send_keys("Raoali1525")
-    time.sleep(1)
+    else:
+        print("Couldn't find input email or password. Try changing Input XPATH")
+
+    dummy[1].send_keys(password)
+
                                         
-    dummy=driver.find_elements(By.XPATH, "//*[@class='sc-gScZFl jsEqJd filled primary m symbol-before ']")
-    dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='sc-gScZFl jlVALW filled primary m symbol-before ']")))
+    dummy=driver.find_elements(By.XPATH, "//*[@class='"+login_button+"']")
+
+    dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='"+login_button+"']")))
     if (len(dummy)>0):
         dummy[0].click()
+        time.sleep(5)
+        try:
+            dummy[0].click()
+        except Exception as e:
+            print("Couldn't find Login Button. Try changing Login Button XPATH")
+        
     else:
-        print("Not Found")
+        print("Couldn't find Login Button. Try changing Login Button XPATH")
+    time.sleep(1)
 
-    # time.sleep(2)
     wait = WebDriverWait(driver, 10)
     try:
-        cookie=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='sc-gScZFl fQiJGU filled primary m symbol-before ']")))
+        cookie=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='"+cookie_button+"']")))
         if (len(cookie)>0):
             cookie[0].click()
         else:
-            print("Nahi mila")
+            print("Couldn't find Cookie Button (Might cause error later on). Try changing Cookie XPATH")
     except Exception as e:
-        print("Cookie not found")
+        print("Couldn't find Cookie Button (Might cause error later on). Try changing Cookie XPATH")
         pass
-    # time.sleep(2)
 
-    # for item in my_list[0]:
     driver.get("https://www.tridge.com/prices")
-    time.sleep(1)
+    wait.until(EC.url_contains('https://www.tridge.com/prices'))
     df = []
     wait = WebDriverWait(driver, 10)
+
     try:
-        butt=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='sc-jVQoqC EPjsG sc-MAqyW eJQUbQ sc-cBQajf gjwVMg']")))
+        butt=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='"+Table_click+"']")))
     except Exception as e:
         print (e)
-        print('button hi ni mila')
+        print("Couldn't find Product Search Dropdown. Try changing XPATH")
+
         return
     try:
         if (len(butt)>0):
             butt[0].click()
         else:
-            print("button Nahi mila")
+            print("Couldn't find Product Search Dropdown. Try changing XPATH")
             return
     except Exception as e:
         print(e)
         return
     try:
-        dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH,"//*[@class='sc-dMVFSy kkcmxE Polaris-TextField__Input']")))
+        dummy=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='"+Table_input+"']")))
     except Exception as e:
-        print("input ni mili")
+        print("Couldn't find Product Search Input. Try changing XPATH")
         return
+
     try:
         if (len(dummy) > 0):
             dummy[0].send_keys(item)
         else:
-            print("Couldn't find input")
+            print("Couldn't find Product Search Input. Try changing XPATH")
             return
     except Exception as e:
         print("Click ni hua")
         return
-    # driver.get("https://www.tridge.com/prices?item_Product=97&entryIds=40860843%2C40880642%2C40862338%2C40861318%2C40860234")
-    time.sleep(1)
     
-    # sc-gScZFl hVfWEN filled white m symbol-before sc-tKebc dkbZPP
     wait = WebDriverWait(driver, 10)
     
     try:
-        prodIndex=wait.until(EC.presence_of_all_elements_located((By.XPATH,"//*[@class='sc-dDPqvT abMMF']")))
+        prodIndex=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='"+Table_first_element+"']")))
     except:
         print (e)
-        print("type k baad click nahi hua")
+        print("Couldn't find Searched Product. Try changing XPATH")
         return
-    
     if (len(prodIndex) > 0):
         prodIndex[0].click()
     else:
-        print("Couldn't find click")
-    time.sleep(1)
+        print("Couldn't find Searched Product. Try changing XPATH")
 
     query_string = "&item_Type=w&item_OriginType=d&interval=w&dateRange=3y&startDate=2020-03-01&endDate=2023-03-17&currency=USD&unit=kg&includeEstimatedPrices=true&includeForecastedPrices=false&"
     driver.get(str(driver.current_url)+query_string)
@@ -121,7 +128,7 @@ def scrape_product(item):
                 table=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='simplebar-content']")))
                 table=wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "table")))
             except Exception as e:
-                print("Table not found")
+                print("Couldn't Find table in element, Please check your internet connection")
                 return
             if (len(table)>0):
                 table[0].click()
@@ -130,17 +137,11 @@ def scrape_product(item):
             actions.send_keys(Keys.PAGE_DOWN).perform()
             actions.send_keys(Keys.PAGE_DOWN).perform()
             time.sleep(1.5)
-            # print (table[0].get_attribute("outerHTML"))
             headings = table[0].find_elements(By.TAG_NAME,"th")
-            # print(headings.text)
-            # for head in headings:
-            #     print(head.text)
-            # print(headings[len(headings)-1].text)
-            # exit()
             try:
                 rows = table[0].find_elements(By.TAG_NAME,"tr")
             except NoSuchElementException:
-                print ("Handling Exception")
+                print ("Unexpected error please debug")
                 continue
             for row in rows:
                 cells = row.find_elements(By.TAG_NAME,"td")
@@ -151,15 +152,10 @@ def scrape_product(item):
                 continue
             else:
                 pass
-                #     print(cell.text,end='  ')
             print(len(rows))
             print("Sr\tCountry\tRegion\tVariety\tPrice")
             for row in rows:
-                if (count>3):
-                    break
                 cells = row.find_elements(By.TAG_NAME,"td")
-                # for cell in cells:
-                #     print(cell.text,end='  ')
                 if (len(cells)>6):
                     print(count,'\t')
                     count+=1
@@ -175,14 +171,10 @@ def scrape_product(item):
                         data['Price Date']=headings[i].text
                         data['Price'] = cells[i].text
                         df.append(data)
-                
-                # print ()
-            # sc-ftTHYK cOLBMg toned secondary s 
-            if (count>3):
+            if (count>1000):
                 break
             actions.send_keys(Keys.PAGE_DOWN).perform()
-            # time.sleep(300)
-            next=driver.find_elements(By.XPATH, "//*[@class='sc-ftTHYK bFoDHH toned secondary s ']")
+            next=wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='sc-ftTHYK bFoDHH toned secondary s ']")))
             if (len(next)>0):
                 if (pageCheck==True):
                     next[0].click()
@@ -191,37 +183,45 @@ def scrape_product(item):
                     if (len(next)>1):
                         next[1].click()
                     else:
-                        print("Scraping ended")
+                        print(item, "Scraping ended")
                         break
             else:
-                print ("Nahi mila")
+                print ("Next Button Not found, Try changing XPATH")
                 break
             time.sleep(1)
             iterate+=1
         except Exception as e:
             print (e)
-            print("As far as i can go.")
+            print("Scarping Finished")
             break
-    # except:
-    #     pass
     dataOut=pd.DataFrame(df)
-
-    print("Yahan end ho gya")
     print(dataOut.head())
     dataOut.to_excel(item+".xlsx")
 
 
 def main():
+    accounts=[("daud.shafqat@tazahtech.com","15531635"),("raoali1525@gmail.com","Raoali1525"),("bazilsb7@gmail.com","Bazil123")]
+    login_inputs="sc-juxSYv daeEXU Polaris-TextField__Input"
+    login_button="sc-gScZFl jlVALW filled primary m symbol-before "
+    Table_click="sc-doKvHv cFuPeG sc-hixjlP lfUcaI sc-jYECHi fOhYPt"
+    Table_input="sc-juxSYv daeEXU Polaris-TextField__Input"
+    Table_first_element="sc-eZceyY eavIQf"
+    cookie_button="sc-gScZFl fQiJGU filled primary m symbol-before "
+
     my_list = []
     with open('my_file.csv', mode='r') as file:
         reader = csv.reader(file)
         for row in reader:
             my_list.append(row)
 
+    # input_tuples = [(item, accounts, login_inputs, login_button, cookie_button, Table_click, Table_input, Table_first_element) for item in my_list[0]]
     # Set up a process pool with the number of processes equal to the number of available CPU cores
-    with mp.Pool(3) as pool:
+    input_tuples = [(item, account, login_inputs, login_button, cookie_button, Table_click, Table_input, Table_first_element) 
+                    for item in my_list[0] for account in accounts]
+    # To use max cpu available mp.Pool(1) - > mo.Pool(mp.cpu_count())
+    with mp.Pool(1) as pool:
         # Use the pool.map() function to run the scrape_product function for each product in parallel
-        pool.map(scrape_product, my_list[0])
+        pool.map(scrape_product, input_tuples)
 
     # Any post-processing or saving of the data here
     
